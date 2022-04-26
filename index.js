1d7bd47fa5e7a78425226e61dd8f8d182410fe96
Moha8ad <bapetnapet@gmail.com>
2022-04-26 15:25:27 +0200

    first version of pictoria is ready to be published

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ import postRoutes from './routes/posts.routes.js';
 
 const app = express();
 
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(cors());
+
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -18,11 +25,6 @@ app.configure(function() {
   app.use(allowCrossDomain);
   //some other code
 }); 
-
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-
-app.use(cors());
 
 app.use('/posts', postRoutes);
 
