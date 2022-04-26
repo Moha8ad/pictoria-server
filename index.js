8a9a53e8d1422d0cc2d08d6bd86839a077593b51
Moha8ad <bapetnapet@gmail.com>
2022-04-26 23:05:50 +0200

    first version of pictoria is ready to be published

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.routes.js';
 
 const app = express();
-dotenv.config();
+
+app.use(cors()); 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use(cors());
 
+dotenv.config();
 
 app.use('/posts', postRoutes);
 
-const CONNECTION = "mongodb+srv://moha8ad:Iq34jgeZe3a1zaPL@bargasht.mjrkw.mongodb.net/pictoriaDB?retryWrites=true&w=majority";
+const MONGO_CONNECTION = "mongodb+srv://moha8ad:Iq34jgeZe3a1zaPL@bargasht.mjrkw.mongodb.net/pictoriaDB?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
