8575e47fb4d6e5934da5bcd3c140e35d681d0386
Moha8ad <bapetnapet@gmail.com>
2022-04-26 17:37:10 +0200

    first version of pictoria is ready to be published

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.routes.js';
 
 const app = express();
-
-
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
+app.use(cors());
 
 
 app.use('/posts', postRoutes);
