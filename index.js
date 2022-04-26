4ebbe211248e21aae8c9658361dc747c56744b8a
Moha8ad <bapetnapet@gmail.com>
2022-04-26 13:39:03 +0200

    first version of pictoria is ready to be published

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.routes.js';
 
 const app = express();
+dotenv.config();
+
+app.use(cors());
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION = "mongodb+srv://moha8ad:Iq34jgeZe3a1zaPL@bargasht.mjrkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION = "mongodb+srv://moha8ad:Iq34jgeZe3a1zaPL@bargasht.mjrkw.mongodb.net/pictoriaDB?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
