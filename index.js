04f5cb5e45d5a228a77435fe3fac03aebfec28d9
Moha8ad <bapetnapet@gmail.com>
2022-04-26 10:47:39 +0200

    first version of pictoria is ready to be published

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 import postRoutes from './routes/posts.routes.js';
 
 const app = express();
-dotenv.config();
 
-app.use(express.json({ limit: '30mb', extended: true}));
-app.use(express.urlencoded({ limit: '30mb', extended: true}));
-
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -19,8 +17,9 @@ app.get('/', (req, res) => {
     res.send('works')
 })
 
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = "mongodb+srv://moha8ad:9tL4v4lP68hxB7Nz@bargasht.mjrkw.mongodb.net/postsDB?retryWrites=true&w=majority";
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect("mongodb+srv://moha8ad:9tL4v4lP68hxB7Nz@bargasht.mjrkw.mongodb.net/postsDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
-    .catch(error=> console.log(error));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
