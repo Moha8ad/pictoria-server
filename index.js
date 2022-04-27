92994ff09243b0afa8ff8e471135048f0f8bcd08
Moha8ad <bapetnapet@gmail.com>
2022-04-27 14:20:23 +0200

    pictoria server is ready

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -18,10 +21,8 @@ app.get('/', (req, res) => {
   res.send('db works')
 })
 
-const MONGO_CONNECTION = "mongodb+srv://moha8ad:rQTMZfsp40ezLhhz@pictoria.mjrkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
