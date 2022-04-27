49fe555bf7bc79231cd8decc558980b9b9216a71
Moha8ad <bapetnapet@gmail.com>
2022-04-27 13:11:54 +0200

    pictoria server is ready

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postRoutes from './routes/posts.routes.js';
+import postRoutes from './routes/posts.js';
 
 const app = express();
-
-app.use(cors()); 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
-app.use('/posts', postRoutes);
+app.use(cors()); 
+
+app.use('/posts', postRoutes)
 
 const MONGO_CONNECTION = "mongodb+srv://moha8ad:rQTMZfsp40ezLhhz@pictoria.mjrkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
