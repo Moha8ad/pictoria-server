533c5e0ed6674a1de43d098daa448aa2705df62a
Moha8ad <bapetnapet@gmail.com>
2022-05-15 12:24:48 +0200

    user auth config added

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,35 +7,23 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => { 
     try {
-        const userMessages = await PostMessage.find();
+        const postMessages = await PostMessage.find();
                 
-        res.status(200).json(userMessages);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
-export const getPost = async (req, res) => { 
-    const { id } = req.params;
-
-    try {
-        const post = await PostMessage.findById(id);
-        
-        res.status(200).json(post);
+        res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
 }
 
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+    const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    const newPostMessage = new PostMessage({ ...post, craetor: req.userId, createdAt: new Date().toISOString( ) })
 
     try {
         await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
@@ -43,11 +31,11 @@ export const createPost = async (req, res) => {
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
+    const { name, title, message, likes, selectedFile, tags } = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+    const updatedPost = { name, title, message, likes, tags, selectedFile, _id: id };
 
     await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
@@ -67,14 +55,23 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id } = req.params;
 
+    if (!req.userId) return  res.json({ message: 'user is not authenticated'})
+ 
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
     
     const post = await PostMessage.findById(id);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    const index = post.likes.findIndex((id) => id === String(req.userId ));
+
+    if (index === -1) {
+        post.likes.push(req.userId)
+    } else {
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     
     res.json(updatedPost);
 }
-
 
 export default router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+
+import authMiddleware from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
-router.post('/', createPost);
-router.get('/:id', getPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post('/', authMiddleware, createPost);
+router.patch('/:id', authMiddleware, updatePost);
+router.delete('/:id', authMiddleware, deletePost);
+router.patch('/:id/likePost', authMiddleware, likePost);
 
 export default router;
